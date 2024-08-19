687c50adba4c27b48ec9cf5f030ddfaa23155ce4
Adrian Américo <1fnadrian1@gmail.com>
2024-08-19 16:06:52 -0300

    chore: add .env

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,25 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-    ]
+    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@": "./src",
+          },
+        },
+      ],
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+        },
+      ],
+    ],
   };
 };
